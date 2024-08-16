b42434e69b1984ece9b0c8c8632b09663ba8f92b
Gulafsha Sayyed <sajidaatique1974@gmail.com>
2024-08-17 00:10:25 +0530

    size of int char float double

---
+++ size.c
@@ -0,0 +1,15 @@
+#include <stdio.h>
+
+int main() {
+    int integerType;
+    char charType;
+    float floatType;
+    double doubleType;
+
+    printf("Size of int: %ld\n", sizeof(integerType));
+    printf("Size of char: %ld\n", sizeof(charType));
+    printf("Size of float: %ld\n", sizeof(floatType));
+    printf("Size of double: %ld\n", sizeof(doubleType));
+
+    return 0;
+}
