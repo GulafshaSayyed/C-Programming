84becefb77e4ca92fb8d1222c7acd2d950f592e2
Gulafsha Sayyed <sajidaatique1974@gmail.com>
2024-08-17 00:00:03 +0530

    quotient remainder

---
+++ ifelse5.c
@@ -0,0 +1,23 @@
+#include<stdio.h>
+int main()
+{
+    int a,b;
+    printf("Enter numerator a:");
+    scanf("%d",&a);
+    printf("Enter denominator b:");
+    scanf("%d",&b);
+    if(b==0)
+    {
+        printf("Denominator cannot be negative .Try Again...");
+    }
+    else
+    {
+        float quotient;
+        int remainder;
+        quotient=a/b;
+        remainder=a%b;
+
+        printf("Quotient of the above two number is %d\n",quotient);
+        printf("Remainder of the above two number is %d",remainder);
+    }
+}
